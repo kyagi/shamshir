f672d46303d100fc9be7fc909b84db53bec69b95
Kazuo Yagi <kazuo.yagi@gmail.com>
2022-02-04 00:31:42 +0900

    Conditional logging when run on github actions

--- src/shamshir-stand-alone.js
+++ src/shamshir-stand-alone.js
@@ -1,4 +1,3 @@
-const Shamshir = require("./shamshir.js")
 const { argv } = require('./yargs.js')
 const { owner, repo, label, quorum, check } = argv
 
@@ -6,6 +5,8 @@ let mode = "live"
 if (check === true) {
     mode = "dry-run"
 }
+
+const Shamshir = require("./shamshir.js")
 const shamshir = new Shamshir(owner, repo, label, quorum, mode)
 
 // TODO: fix or use top level await
